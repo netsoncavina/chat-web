a808b6dd337ffd3f52f2f5c813a827d8529f21e8
Netson Cavina <netsoncavina@gmail.com>
2024-08-15 17:26:49 -0300

    initial config to joinRoom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 
+import { useState } from 'react'
 import './App.css'
 import io from 'socket.io-client'
 
 const socket = io('http://localhost:3001')
 
 function App() {
+   const [name, setName] = useState('')
+   const [roomId, setRoomId] = useState('')
+
+  const joinRoom = () => {
+    if (name && roomId) {
+      socket.emit('joinRoom', roomId)
+    }
+  }
 
   return (
     <>
      <h1>Chat App</h1>
+
+      <div>
+        <input type="text" placeholder="Enter your name"  onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder="Enter your room id"  onChange={(e) => setRoomId(e.target.value)} />
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+
+
     </>
   )
 }
